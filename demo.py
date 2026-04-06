5aa549ca5b3a98d1f2864a31817267f47010abc1
anujjj13 <anujsharma1913@gmail.com>
2026-04-06 14:43:30 +0530

    Initial commit

---
+++ demo.py
@@ -0,0 +1,14 @@
+# Simple Python program
+
+# take input from user
+name = input("Enter your name: ")
+
+# print output
+print("Hello,", name)
+
+# simple calculation
+a = 10
+b = 5
+sum_result = a + b
+
+print("Sum of", a, "and", b, "is:", sum_result)
